9f874052ed4956e4ae2ac98f22b081e8eab43548
zd <yobaci@163.com>
2025-04-25 11:29:28 +0800

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-   formatters: true,
+  formatters: true,
   react: true,
+}, {
+  ignores: [
+    './packages/web/public/mockServiceWorker.js',
+  ],
 })
--- packages/web/src/pages/home.tsx
+++ packages/web/src/pages/home.tsx
@@ -1,10 +1,35 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useRef } from 'react'
 import PageWrapper from '../layout/page'
+import Button from '~/components/button'
 
 export default function Home() {
+
+  const ws = useRef<WebSocket|null>(null)
+  useEffect(()=>{
+    if(!ws.current){
+      ws.current = new WebSocket('')
+      ws.current.addEventListener('open',()=>{
+        console.log('open')
+      })
+    }
+    return ()=>{
+      if(ws.current){
+        ws.current?.close()
+        ws.current = null
+      }
+    }
+  },[])
+  
+  function handlePrint(){
+    console.log('print')
+  }
+
   return (
     <PageWrapper className="flex justify-center flex-col items-center">
-      <Link to="/reference" className="text-lg font-bold">reference</Link>
+      <Button type='button' onClick={handlePrint}>
+        打印
+      </Button>
+      {/* <Link to="/reference" className="text-lg font-bold">reference</Link> */}
     </PageWrapper>
   )
 }
